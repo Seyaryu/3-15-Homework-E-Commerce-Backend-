010fe9c0501c679d3aef896172b1782a1366d75b
Daniel Holland <danhol1989@gmail.com>
2022-03-13 13:02:44 -0700

    finished category-routes api.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,23 +6,55 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll().then((categories) => {
+    res.json(categories);
+  });
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+  })
+  .then((categoryData) => {
+    res.json(categoryData);
+  }); 
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.bulkCreate([
+    {
+      id: req.params.id,
+      category_name: req.params.category_name,
+    }
+  ])
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+      id: req.body.id,
+      category_name: req.body.category_name,
+    }
+  )
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      category_id: req.params.category_id,
+    },
+  })
+  .then ((deletedCategory) => {
+    res.json(deletedCategory);
+  })
+  .catch((err) => res.json(err));
 });
 
 module.exports = router;
